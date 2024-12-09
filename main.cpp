c3548eb04d088f16327c4910a5d2dfb726290e6a
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-08 18:55:52 -0800

    Complete Part One

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum_ascii(const string &s);
+
 int main()
 {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
+    cout << "The ASCII sum of AB is " << sum_ascii("AB") << "\n";
+
+//    char a = 'A';
+//    cout << a << endl;
+//    cout << (int) a << endl;
+//    int b = 66;
+//    cout << b << endl;
+//    cout << (char) b << endl;
 
     return 0;
 }
@@ -20,3 +24,12 @@ These targets are present in the dataset and can be used for testing:
 666D109AA22E
 E1D2665B21EA
 */
+
+int sum_ascii(const string &s)
+{
+    int sum = 0;
+    for (char c : s)
+        sum += (int) c;
+
+    return sum;
+}
