8ec5c537276c0df3c9cb81ebd718348c82cdbcc3
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-08 19:09:05 -0800

    Complete Part Two

--- main.cpp
+++ main.cpp
@@ -6,16 +6,25 @@ int sum_ascii(const string &s);
 
 int main()
 {
-    ifstream fin("lab-37-data.txt");
-    if (!fin.good())
-        throw "I/O error";
+    try
+    {
+        ifstream fin("lab-37-data.txt");
+        if (!fin.good())
+            throw "I/O error";
+
+        string line;
+        long long grandTotal = 0;
+        while (getline(fin, line))
+        {
+            int asciiSum = sum_ascii(line);
+            grandTotal += asciiSum;
+        }
+        cout << "The grand sum is " << grandTotal << "\n";
+    }
     catch (const char* e)
     {
         cout << e << '\n';
     }
-
-    string line;
-
 
     return 0;
 }
