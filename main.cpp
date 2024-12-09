342ddd664573cec159b78b035d2e4004c2037dde
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-12-08 19:50:22 -0800

    10-minute update

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 using namespace std;
 
-int sum_ascii(const string &s);
+const int CAPACITY = 10000;
+
+int get_hash_index(const string &, const int);
 
 int main()
 {
@@ -12,14 +14,25 @@ int main()
         if (!fin.good())
             throw "I/O error";
 
+        map<int, list<string>> hashTable {};
+
         string line;
-        long long grandTotal = 0;
         while (getline(fin, line))
         {
-            int asciiSum = sum_ascii(line);
-            grandTotal += asciiSum;
+            int hash_index = get_hash_index(line);
+            hashTable[hash_index].push_back(line);
         }
-        cout << "The grand sum is " << grandTotal << "\n";
+        fin.close();
+
+        cout << "Displaying the first 100 hash table entries:\n";
+
+        int displayCount = 0;
+        for (auto it = hashTable.begin(); it != hashTable.end() && displayCount < 100; it++, displayCount++)
+        {
+            cout << "Hash Index " << it->first << ":\n";
+            for (const auto &code : it->second)
+                cout << "  " << code << "\n";
+        }
     }
     catch (const char* e)
     {
@@ -37,11 +50,20 @@ These targets are present in the dataset and can be used for testing:
 E1D2665B21EA
 */
 
-int sum_ascii(const string &s)
+
+/*****************************************************************
+ * FUNCTION: A typical modular arithmetic based hash function that encrypts a string
+ *
+ * @param s
+ * @param capacity
+ *
+ * @return INT;
+ */
+int get_hash_index(const string &s, const int capacity = CAPACITY)
 {
     int sum = 0;
     for (char c : s)
         sum += (int) c;
 
-    return sum;
+    return sum % capacity;
 }
